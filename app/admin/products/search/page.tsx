fe86dc1c7a1e2b0184c2a9a689a82134f504d7df
Julian <julian.trujillo@focunti.com>
2025-06-06 15:53:58 -0500

    fix

--- app/admin/products/search/page.tsx
+++ app/admin/products/search/page.tsx
@@ -1,40 +1,45 @@
+import { ProductsWhitCategory } from "@/app/admin/products/page"
 import ProductSearchForm from "@/components/products/ProductSearchForm";
 import ProductTable from "@/components/products/ProductTable";
 import Heading from "@/components/ui/Heading";
-import Prisma from "@/src/lib/prisma"
+import prisma from "@/src/lib/prisma";
 
-async function searchProducts(searchTerm: string){
-    return await Prisma.product.findMany({
-        where: {
-            name: {
-                contains: searchTerm,
-                mode: 'insensitive'
-            }
-        },
-        include: {
-            category: true
-        }
-    })
-} 
+async function searchProducts(searchTerm: string) {
+   const products = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: searchTerm,
+        mode: "insensitive"
+      },
+    },
+    include: {
+      category: true
+    }
+  });
 
-export default async function SearchPage({ searchParams }: Readonly<{ searchParams: Promise<{ search: string }> }>) {
-    const search = (await searchParams).search
-    const products = await searchProducts(search)
+  return products as ProductsWhitCategory
+}
+
+export default async function SearchPage({
+  searchParams,
+}: Readonly<{ searchParams: Promise<{ search: string }> }>) {
+  const search = (await searchParams).search;
+  const products = await searchProducts(search);
   return (
     <>
-        <Heading>Resultados de la búsqueda: { search }</Heading>
+      <Heading>Resultados de la búsqueda: {search}</Heading>
 
-        <div className="flex flex-col lg:flex-row gap-5 justify-end">
-            <ProductSearchForm />
-        </div>
+      <div className="flex flex-col lg:flex-row gap-5 justify-end">
+        <ProductSearchForm />
+      </div>
 
-        { products.length ? (
-            <ProductTable 
-                products={products}
-            />
-        ) : (
-            <p className="text-center text-lg">No se encontraron resultados para la búsqueda: {search}</p>
-        )}
+      {products.length ? (
+        <ProductTable products={products} />
+      ) : (
+        <p className="text-center text-lg">
+          No se encontraron resultados para la búsqueda: {search}
+        </p>
+      )}
     </>
-  )
+  );
 }
